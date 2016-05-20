f34a12c44aaa5d1783bc6bc8c583d8cd662cb0bc
vandersonmaroni <vandersongmaroni@gmail.com>
2016-05-20 13:16:49 -0300

    Instalando plugin do sass  e adicionando task dele no gulp

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('hello',function(){
+  console.log("Hello Zell");
+});
+
+gulp.task('sass',function(){
+  return gulp.src('app/scss/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+});
